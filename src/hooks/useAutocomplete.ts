b0bc2d2e70e88cfdf6fd8a1824cda5acae287b5f
Olle <olle.johansson@avaloninnovation.com>
2024-02-23 13:28:47 +0100

    removed potential error msg in autocomplete hook

--- src/hooks/useAutocomplete.ts
+++ src/hooks/useAutocomplete.ts
@@ -3,7 +3,7 @@ import { useQuery } from "react-query";
 export const useAutocomplete = (input: string) => {
   const { data, isLoading, error } = useQuery(["autocomplete", input], () => {
     return (
-      input.length > 0 &&
+      input?.length > 0 &&
       fetch(
         `https://api.movies.dcts.se/rpc/movies_autocomplete?q=${input}&limit=5`
       ).then((res) => res.json())
