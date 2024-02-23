bdfb06bb35a1ff546e5977a9834b01b5f293b7c7
Olle <olle.johansson@avaloninnovation.com>
2024-02-23 13:26:02 +0100

    removed bug where suggestion wouldnt disappear

--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useAutocomplete } from "../../hooks/useAutocomplete";
 import style from "./index.module.css";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -13,6 +14,8 @@ export const Searchbar = () => {
 
   const { data } = useAutocomplete(watch("search"));
 
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
   const onSubmit: SubmitHandler<any> = (data) => console.log(data);
 
   const handleItemClick = (item: string) => {
@@ -25,10 +28,17 @@ export const Searchbar = () => {
 
   return (
     <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Search..." {...register("search")} />
+      <input
+        type="text"
+        placeholder="Search..."
+        {...register("search")}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
       <button type="submit">Search</button>
       <div>
         {data &&
+          isFocused &&
           data.map((item: string) => {
             return (
               <button type="submit" onClick={() => handleItemClick(item)}>
